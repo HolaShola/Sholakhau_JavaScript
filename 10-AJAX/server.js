a4b6e744357c30357926991251fe4d3f91c9246b
HolaShola <xxjxxx@mail.ru>
2016-09-08 01:19:28 +0300

    task10

---
+++ 10-AJAX/server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var url = require('url');
+var static = require('node-static');
+var file = new static.Server('.', {
+  cache: 0
+});
+function accept(req, res) { 
+      file.serve(req, res);
+}
+
+http.createServer(accept).listen(8080);
+
+console.log('hello from port 8080');
